643224af1ff9d46a97814da6f147566622508b6f
Dibyendu Sikdar <dibyendu.coder@gmail.com>
2017-11-04 03:23:51 +0530

    Update set1_1.py

--- set1_1.py
+++ set1_1.py
@@ -1,4 +1,4 @@
 import base64
 import binascii
 encoded = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
-base64.b64encode(binascii.unhexlify(encoded)) == 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
+print base64.b64encode(binascii.unhexlify(encoded)) == 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
